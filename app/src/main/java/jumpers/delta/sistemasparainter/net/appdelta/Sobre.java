aa67f381bc0023d85e7e72dc21d731efef079b27
BRENO FERREIRA SENA <breno.fsena@senacsp.edu.br>
2017-04-19 22:04:12 -0300

    Layout tela sobre

--- app/src/main/java/jumpers/delta/sistemasparainter/net/appdelta/Sobre.java
+++ app/src/main/java/jumpers/delta/sistemasparainter/net/appdelta/Sobre.java
@@ -3,12 +3,11 @@ package jumpers.delta.sistemasparainter.net.appdelta;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-
-public class MainActivity extends AppCompatActivity {
+public class Sobre extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sobre);
     }
 }
